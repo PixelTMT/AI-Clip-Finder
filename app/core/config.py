3cf3f6029d1b5600097ea8ef969af3c52ed09fcc
Kenz <48181402+PixelTMT@users.noreply.github.com>
2026-03-16 00:15:21 +0700

    increase upload host limit

--- app/core/config.py
+++ app/core/config.py
@@ -15,7 +15,7 @@ class Settings:
     POLLINATIONS_APP_KEY = os.environ.get("POLLINATIONS_APP_KEY", "")
     # Hosting & Limits
     HOSTING = os.environ.get("HOSTING", "false").lower() == "true"
-    MAX_FILE_SIZE = 100 * 1024 * 1024  # 100MB
+    MAX_FILE_SIZE = 500 * 1024 * 1024  # 500MB
     PROJECT_EXPIRY_DAYS = 30
     USER_ID_COOKIE = "user_id"
     def __init__(self):
